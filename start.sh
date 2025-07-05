cb49c80c3a676f3ffb14848ad326505b62c7ac11
Pierre Awaragi <pierre@awaragi.com>
2025-07-05 00:51:07 -0400

    add: script to start application with retries and configurable sleep interval

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Default sleep interval in seconds
+SLEEP=300
+
+if [ ! -f .env ]; then
+    echo "Error: .env file not found!"
+    exit 1
+fi
+
+while true
+do
+  sudo -u debian-spamd env "PATH=$PATH" "NODE_PATH=$NODE_PATH" npm start
+  echo "Waiting for $SLEEP"
+  sleep $SLEEP
+done
