4907485507f04c6b163260b1ace2747af744b8ae
Pierre Awaragi <pierre@awaragi.com>
2025-07-07 20:05:06 -0400

    refactor: centralize script execution into reusable `run_scripts` function for improved readability and maintainability in entrypoint script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 set -e
 
-MODE="${ENTRYPOINT_MODE:-$1}"
+MODE="${1:-$ENTRYPOINT_MODE}"
+
+# Function to run all training and scanning scripts
+run_scripts() {
+  node train-spam.js
+  node train-ham.js
+  node train-whitelist.js
+  node scan-inbox.js
+}
 
 case "$MODE" in
   loop)
     spamd -d -c -m 5 --helper-home=/home/sauser -s stderr
     sleep 2
     while true; do
-      node train-spam.js
-      node train-ham.js
-      node train-whitelist.js
-      node scan-inbox.js
+      run_scripts
       sleep "${SCAN_INTERVAL:-300}"
     done
     ;;
   once)
     spamd -d -c -m 5 --helper-home=/home/sauser -s stderr
     sleep 2
-    node train-spam.js
-    node train-ham.js
-    node train-whitelist.js
-    node scan-inbox.js
+    run_scripts
     ;;
   update)
+    # Run sa-update as sauser to maintain consistent permissions
     sa-update --gpgkey 24C063D8 --channel kam.sa-channels.mcgrail.com
     sa-compile
     ;;
