4f84ea495dc003f7f82952a33f3617897643badb
Pierre Awaragi <pierre@awaragi.com>
2025-07-08 00:20:11 -0400

    refactor: remove redundant healthcheck option from runtime scripts for improved clarity

--- bin/run-bash-root.sh
+++ bin/run-bash-root.sh
@@ -3,6 +3,6 @@ set -e
 
 # Launch the image and run interactive bash
 echo "Launching spam-scanner container with interactive bash..."
-docker run --rm -it --user root --name spam-scanner-shell-root -v spamassassin-data:/home/sauser/.spamassassin --health-cmd='none' --entrypoint /bin/bash spam-scanner:latest
+docker run --rm -it --no-healthcheck --user root --name spam-scanner-shell-root -v spamassassin-data:/home/sauser/.spamassassin --entrypoint /bin/bash spam-scanner:latest
 
 # Note: The container will be removed when you exit the bash shell
--- bin/run-update.sh
+++ bin/run-update.sh
@@ -3,5 +3,5 @@ set -e
 
 # Run the update command using root user
 echo "Running SpamAssassin update as root user..."
-docker run --rm --name spamassassin-updater --user root -v spamassassin-data:/home/sauser/.spamassassin --health-cmd='none' spam-scanner:latest update
+docker run --rm --no-healthcheck --user root --name spamassassin-updater -v spamassassin-data:/home/sauser/.spamassassin spam-scanner:latest update
 echo "Update completed successfully!"
